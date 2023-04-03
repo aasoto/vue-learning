80d17fba1d92bc567f9aaaff076dedeb931a667a
aasoto <andresalfredosotosuarez@gmail.com>
2023-04-03 18:07:23 -0500

    feat: Vue Router (Composition API)

--- router-1/src/views/PokeView.vue
+++ router-1/src/views/PokeView.vue
@@ -1,5 +1,40 @@
 <script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import { useRoute, useRouter } from "vue-router";
+
+// sirve para acceder a los parametros de la ruta
+const route = useRoute();
+const router = useRouter();
+
+const poke = ref([]);
+
+const back = () => {
+  router.push('/pokemons');
+}
+
+const getData = async () => {
+  try {
+    const { data } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
+    );
+    console.log(data);
+    poke.value = data;
+  } catch (error) {
+    console.log(error);
+    poke.value = null;
+  }
+}
+
+getData();
 </script>
 <template>
-  <h1>Pokemon: {{ $route.params.name }}</h1>
+  <div v-if="poke">
+    <img :src="poke.sprites?.front_default" alt="">
+    <h1>Pokemon: {{ $route.params.name }}</h1>
+  </div>
+  <h1 v-else>No existe Pokemon</h1>
+  <button @click="back" class="btn btn-outline-primary">
+    Volver
+  </button>
 </template>
