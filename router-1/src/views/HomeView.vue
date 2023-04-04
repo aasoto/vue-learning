03897cbcee9f68cf278f7a2b5d218c67a18230a2
aasoto <andresalfredosotosuarez@gmail.com>
2023-04-04 10:25:26 -0500

    feat: primer store con pinia

--- router-1/src/views/HomeView.vue
+++ router-1/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '../store/counter';
+
+const useConter = useCounterStore();
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home counter: {{ useConter.count }}</h1>
+    <h2>Double: {{ useConter.double }}</h2>
+    <button @click="useConter.increment">Increment</button>
   </main>
 </template>
